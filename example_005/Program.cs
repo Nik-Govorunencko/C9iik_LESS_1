3f781768cbe7a4331a381bfe3d85286d31be92da
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-01 09:08:40 -0400

    example_005

---
+++ example_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "никита")
+{
+    Console.WriteLine("Ты опять выходишь на связь?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
