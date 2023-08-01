63c47fb71f1a0027cd2d8554c462c77ac2383c55
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-01 16:15:12 -0400

    seminar_ex_005

---
+++ sem_ex_005/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите трёхзначное число, и мы покажем вам еденицы этого числа");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 0)
+{
+    number = number * -1;
+}
+
+if(number < 1000)
+{
+    if(number > 99)
+    {
+        number = number % 10;
+        Console.WriteLine(number + " - еденицы");
+    }
+    else
+    {
+        Console.WriteLine("Укажите трехзначное число!");
+    }
+}
+else
+{
+    Console.WriteLine("Укажите трехзначное число!");
+}
+
+
