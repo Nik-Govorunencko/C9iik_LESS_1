9d09beaef7ce82ad6ea2f06e29380db86575d7aa
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-01 13:55:15 -0400

    seminar_ex_001

---
+++ sem_ex_001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Напишите число, которое хотите возвести в квадрат: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.Write("Квадрат этого числа: " + result);
+
+
