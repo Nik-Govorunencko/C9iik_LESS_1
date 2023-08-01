f8164202fbe58e5998c8072e40c801ea248aa6bb
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-01 15:35:44 -0400

    seminar_ex_004

---
+++ sem_ex_004/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите число, и мы покажим все целые числа между ним и его противоположностью.");
+int number_pl = Convert.ToInt32(Console.ReadLine());
+int number = number_pl;
+int number_min = number_pl * -1;
+if(number_pl < 0)
+{
+    number_pl = number * -1;
+}
+
+int count_all = number_pl * 2;
+int count = 0;
+
+
+
+if(number_pl > 0)
+{
+    Console.Write(number_pl + ", ");
+    while(count < count_all)
+    {
+        number_pl = number_pl - 1;
+        count++;
+        Console.Write(number_pl + ", ");
+    }
+}
+else if(number_pl < 0)
+{
+    Console.Write(number_pl + ", ");
+    while(count < count_all)
+    {
+        number_pl = number_pl + 1;
+        count++;
+        Console.Write(number_pl + ", ");
+    }
+}
+else if (number_pl == 0)
+{
+    Console.WriteLine(0);
+}
