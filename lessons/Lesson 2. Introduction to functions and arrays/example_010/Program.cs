817466caa734c764680ae60ad1bb62447ebc4e00
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-04 06:36:03 -0400

    example_010

---
+++ lessons/Lesson 2. Introduction to functions and arrays/example_010/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {23, 12, 54, 7, 2, 84, 2, 84, 82};
+
+int find = 84;
+int index = 0;
+
+while(index < array.Length)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
