e7f47b6b9e96e4545e9435782fb9eceaa58f3db7
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-04 07:10:43 -0400

    example_011

---
+++ lessons/Lesson 2. Introduction to functions and arrays/example_011/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int size = col.Length;
+    int count = 0;
+    while(count < size)
+    {
+        Console.Write(col[count] + " ");
+        count++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
