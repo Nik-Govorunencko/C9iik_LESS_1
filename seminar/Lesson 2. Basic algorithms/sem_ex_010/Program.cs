8ef4a2ceb09d1932b23722ac0cabdecdb8a7d017
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-04 14:24:07 -0400

    sem_ex_010

---
+++ seminar/Lesson 2. Basic algorithms/sem_ex_010/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число и мы покажем вам кратно ли оно 7 и 23.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Это число является кратным 7 и 23.");
+}
+else
+{
+    Console.WriteLine("Это число не является кратным 7 и 23");
+}
