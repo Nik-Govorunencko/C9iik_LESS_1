a084470f70ed09e2846a119c61f6537d6d80a53d
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-04 13:42:16 -0400

    sem_ex_006

---
+++ seminar/Lesson 2. Basic algorithms/sem_ex_006/Program.cs
@@ -0,0 +1,19 @@
+﻿Random rand = new Random();
+int number = rand.Next(10, 100);
+
+int a = number / 10;
+int b = number % 10;
+
+if(a > b)
+{
+    Console.Write("Наибольшее число:" + a + ", наименьшее: " + b);
+}
+else if(b > a)
+{
+    Console.Write("Наибольшее число: {0}, наименьшее {1}", b, a);
+}
+else
+{
+    Console.Write($"Оба числа одинаковые: {a} = {b}");
+}
+
