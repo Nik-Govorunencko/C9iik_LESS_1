4c8f242720cd1ed8b00c51d2580da770bd69a017
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-04 14:01:13 -0400

    sem_ex_008

---
+++ seminar/Lesson 2. Basic algorithms/sem_ex_008/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите два числа, и мы покажем является ли одно число квадратом другого.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 * number1 == number2)
+{
+    Console.Write("Число {0} является квадратом числа {1}.", number1, number2);
+}
+else if(number2 * number2 == number1)
+{
+    Console.Write("Число {0} является квадратом числа {1}.", number2, number1);
+}
+else
+{
+    Console.Write("Ни одно число не является квадратом другого.");
+}
+// но также можно было просто использовать || - или
