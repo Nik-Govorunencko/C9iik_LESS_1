f45d5450dfa60afbf04ec1c9100d3414d5ab5b96
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-04 14:29:11 -0400

    sem_ex_009_adds

--- seminar/Lesson 2. Basic algorithms/sem_ex_009/Program.cs
+++ seminar/Lesson 2. Basic algorithms/sem_ex_009/Program.cs
@@ -1,12 +1,13 @@
 ﻿Console.WriteLine("Введите два числа и мы покажем является ли первое число кратным второму.");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = number1 % number2;
 
-if(number1 % number2 == 0)
+if(number3 == 0)
 {
     Console.Write("Первое число является кратным второму");
 }
 else
 {
-    Console.Write("Первое число не является кратным второму");
+    Console.Write($"Первое число не является кратным второму, остаток: {number3}");
 }
